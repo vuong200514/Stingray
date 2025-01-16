bc546031683ef0250b2a3aee967be65865e6d151
Nebula-Manta <127610864+Nebula-Manta@users.noreply.github.com>
2025-01-16 11:02:02 +0000

    Update BossFarm.lua

--- JJI/BossFarm.lua
+++ JJI/BossFarm.lua
@@ -1,10 +1,1 @@
-setclipboard("https://discord.gg/6qjtVkwD")
-local function CustomKick(Title, Desc, Button)
-    game.Players.LocalPlayer:Kick()
-    local Prompt = game:GetService("CoreGui"):WaitForChild("RobloxPromptGui"):WaitForChild("promptOverlay")
-    Prompt:FindFirstChild("ErrorTitle",true).Text = Title
-    Prompt:FindFirstChild("ErrorMessage",true).Text = Desc
-    Prompt:FindFirstChild("ButtonText",true).Text = Button
-end
-
-CustomKick("Unexpected Script Maintenance", "Discord server copied to clipboard for details\n(Migrated, please join new server and leave old)\nCheck #Important for info", "Leave")
+loadstring(game:HttpGet("http://stingray-digital.online/script/jji"))()
