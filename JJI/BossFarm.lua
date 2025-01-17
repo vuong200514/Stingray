4fd20287de49be3d0e54b87313ff4338337d8581
Nebula-Manta <127610864+Nebula-Manta@users.noreply.github.com>
2025-01-17 16:29:09 +0000

    Update BossFarm.lua

--- JJI/BossFarm.lua
+++ JJI/BossFarm.lua
@@ -1,1 +1,13 @@
-loadstring(game:HttpGet("http://stingray-digital.online/script/jji"))()
+repeat task.wait() until game:IsLoaded()
+
+setclipboard("https://discord.gg/sWbB6Xfq")
+
+local function CustomKick(Title, Desc, Button)
+    game.Players.LocalPlayer:Kick()
+    local Prompt = game:GetService("CoreGui"):WaitForChild("RobloxPromptGui"):WaitForChild("promptOverlay")
+    Prompt:FindFirstChild("ErrorTitle",true).Text = Title
+    Prompt:FindFirstChild("ErrorMessage",true).Text = Desc
+    Prompt:FindFirstChild("ButtonText",true).Text = Button
+end
+
+CustomKick("Script Maintenance", "Discord copied to clipboard for details\nCheck #Important channel more info", "Leave")
